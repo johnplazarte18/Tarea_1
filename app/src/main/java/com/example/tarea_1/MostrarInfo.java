91b45fe760b4fed41c9c19664926d8ed8dacf5f6
jhonp <john.plazarte2017@uteq.edu.ec>
2020-11-23 22:23:51 -0500

    Actualización #2 23-11-2020

---
+++ app/src/main/java/com/example/tarea_1/MostrarInfo.java
@@ -0,0 +1,14 @@
+package com.example.tarea_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_info);
+    }
+}
